35e6e76b7aa364c03217ad208f45d58c485922b5
nayakprakash@gmail.com <nayakprakash@gmail.com>
2016-03-24 20:36:21 -0400

    grep1

--- CSA.R
+++ CSA.R
@@ -2,13 +2,14 @@ install.packages("dplyr")
 install.packages("tidyr")
 install.packages("ggplot2")
 install.packages("ggvis")
-
+install.packages("lubridate")
 library (dplyr)
 library (tidyr)
 library (ggplot2)
 library (ggvis)
+library (lubridate)
 
-csatest1<-read.csv("C:/Users/nayakp//research/Rspace/CSA.csv",header = T, sep = ",",stringsAsFactors = F)#this read.csv works well
+csatest<-read.csv("C:/Users/nayakp//research/Rspace/CSA.csv",header = T, sep = ",",stringsAsFactors = F)#this read.csv works well
 # / slash is used for R code, \ this one for windows native address
 which(csatest1$Age.at.Surgery.<=35)
 ifelse(csatest1$Age.at.Surgery.<=35,"young","old")->csatest1$agesplitat35#I was super happy at being able to parse the data by age and add a young/old column in one elegant step!
@@ -18,17 +19,13 @@ subset(csatest1,agesplitat35=="young")->csayoung
 subset(csatest1,agesplitat35=="old")->csaold
 csatest1$agesplitat35<-as.factor(csatest1$agesplitat35)#since young and old were characters
 write.table(csa,file="C:/Users/nayakp/research/Rspace/csa.csv",row.names=F)#saves output to a txt or csv file but doesn't render well with View function later
-csa<-csatest[,1:52]
-csa<-csa[,-(1:4)]
-library (tidyr)
-install.packages("lubridate")
-library(lubridate)
-csa$Date.of.Birth<-dmy(csa$Date.of.Birth)
-csa$Biopsy.Date<-dmy(csa$Biopsy.Date)
-csa$Surgery.Date<-dmy(csa$Surgery.Date)
-colnames(csa)->col.names
-csadate<-csa[,c(grepl("Date.",x= col.names)==TRUE)]
-str(csadate)
-dmy(csa$c(grepl("Date.",x= col.names)==TRUE))
-csaposix<-lapply(with(csa,csa[,c(grepl("Date.",x= col.names)==TRUE)]),dmy)
-str(csaposix)
+
+csa<-csatest[,7:52]#remove unwanted columns
+
+# coerce POSIXct to all columns with date
+csa[,grep("Date",names(csa))]<-lapply(csa[,grep("Date",names(csa))],dmy)
+csa<-csa[,-9]
+csa<-csa[,-5]
+csa$Grade.<-as.factor(csa$Grade.)
+names(csa)<-c("dob","age_surgery","sex","diagnosis","site","anatomy","presenting_status","date_biopsy","prior_surgery","type_surgery","date_surgery","type_closure","radiation","chemotherapy","size","grade","margin","necrosis","complications","surgery_complications","date_surgery_complications","relapse1_type","relapse1_date","relapse1_type_surgery","relapse1_date_surgery","relapse1_radiation","relapse1_chemotherapy","relapse2_type","relapse2_date","relapse2_type_surgery","relapse2_date_surgery","relapse2_radiation","relapse2_chemotherapy","relapse3_type","relapse3_date","relapse3_type_surgery","relapse3_date_surgery","relapse3_radiation","relapse3_chemotherapy","status","date_status","dfs_months","os_months","comments")
+write.csv(csa,file="csvposix.csv",row.names=F)
